7d86a4e9164b517d39e4c43c80802ca998023046
shennana <shennana@jd.com>
2019-04-15 14:42:42 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import _ from 'lodash';
+
+export function aUiq(arr) {
+  return _.uniq(arr)
+}
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+var path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'amies.js',
+    library: 'amies',
+    libraryTarget: 'umd'
+  },
+  externals: {
+    lodash: {
+      commonjs: 'lodash',
+      commonjs2: 'lodash',
+      amd: 'lodash',
+      root: '_'
+    }
+  }
+};
